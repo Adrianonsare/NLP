5ba0079334b73d2758613e8d66b93148d5c05e35
Adrianonsare <adrianonsare@gmail.com>
2022-03-21 00:20:26 +0300

    gitgnore

--- SentimentAnalysis/app.py
+++ SentimentAnalysis/app.py
@@ -2,34 +2,18 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+import config
+import tweepy
 
-#bearer_token="AAAAAAAAAAAAAAAAAAAAALe2aQEAAAAA90C5FoIsZ%2BUS9iU1q4AHo4AApnw%3DOoZPqcsyR1qFEi9oH51Es3lkngN3wegmyY00LIB3KQOUme4nJv"
-consumer_key="f7Bm3Zi4eiN4O7QGzE0O6j1yT"
-consumer_secret="dAUBbArJ3f2zJqQlMpFTVuJ8mON31AylmnRbJ2vl8ZasG4m3MX"
-access_token="1683252618-Jc28x9poY0WQlOwlhfIDYYcDducDjrAmg0rr8pm"
-access_token_secret="MnT2x5GRrLq7bRllhmSFOwsy3NiYpP8wsHOGCU7Ui5RhZ"
+# calling a client
+client = tweepy.Client(
+    consumer_key=config.consumer_key,
+    consumer_secret=config.consumer_secret,
+    access_token=config.access_token,
+    access_token_secret=config.access_token_secret
+)
 
-
-#This is a basic listener that just prints received tweets to stdout.
-class StdOutListener(StreamListener):
-
-def on_data(self, data):
-    print data
-    return True
-
-def on_error(self, status):
-    print status
+response = client.create_tweet(text='twitter api testoooor')
 
 
-if __name__ == '__main__':
-
-#This handles Twitter authetification and the connection to Twitter 
-Streaming API
-l = StdOutListener()
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-stream = Stream(auth, l)
-
-#This line filter Twitter Streams to capture data by the keywords: 'python', 
-'javascript', 'ruby'
-stream.filter(track=['python', 'javascript', 'ruby'])
+print(response)
