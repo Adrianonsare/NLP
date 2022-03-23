553a44b756e5f30c9dc45498bec1b9fe0773002d
Adrianonsare <adrianonsare@gmail.com>
2022-03-23 11:36:16 +0300

    updATE

--- SentimentAnalysis/app.py
+++ SentimentAnalysis/app.py
@@ -1,22 +1,7 @@
-#Import the necessary methods from tweepy library
-
-import config
 import tweepy
+import config
 
-bearer_token =config.bearer_token
-
-client = tweepy.Client(bearer_token)
-
-# Get Tweets
-
-# This endpoint/method returns a variety of information about the Tweet(s)
-# specified by the requested ID or list of IDs
-
-tweet_ids = [1460323737035677698, 1293593516040269825, 1293595870563381249]
-
-# By default, only the ID and text fields of each Tweet will be returned
-# Additional fields can be retrieved using the tweet_fields parameter
-response = client.get_tweets(tweet_ids, tweet_fields=["created_at"])
-
-for tweet in response.data:
-    print(tweet.id, tweet.created_at)
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+auth.set_access_token(accessToken, accessTokenSecret)
+api = tweepy.API(auth)
